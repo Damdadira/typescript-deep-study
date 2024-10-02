52399b1c7bff65e43d92407756b5f3f8b4bce88d
duddk <duddk@scatterx.io>
2024-10-02 10:29:30 +0900

    연습중

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Greetings from './Greetings'
 
 function App() {
   return (
-    <div className='bg-green-400'>
-      <div className='text-3xl font-bold underline'>hi</div>
-    </div>
+    <>
+      <div className='bg-green-400'>
+        <div className='text-3xl font-bold underline'>hi</div>
+      </div>
+      <Greetings name='hi'></Greetings>
+    </>
   )
 }
 
